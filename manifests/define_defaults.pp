552706c7ad5310acb885811986016da9bc398792
Matt Ullmer <meu@yelp.com>
2014-01-24 16:31:55 -0800

    adding a define and a defaults file that are useful to set up explicit backups

--- manifests/define_defaults.pp
+++ manifests/define_defaults.pp
@@ -5,10 +5,15 @@
 # innodb_log_group_home_dir or other things explicitly
 #
 define xtrabackup::define_defaults(
+  $user,
+  $pass,
+  $port,
+  $socket,
+  $scriptdir,                # The directory to stash the cnf
   $datadir,                  # The main mysql directory
   $innodb_log_group_home_dir # The innodb log file dir
 ) {
-  file { "":
+  file { "${scriptdir}/${title}":
     owner   => 'root',
     group   => 'root',
     mode    => 644,
