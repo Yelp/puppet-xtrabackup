e09b79f1cb713b017ad97bc213c4c7ea5b088c84
Matt Ullmer <meu@yelp.com>
2014-01-30 14:30:21 -0800

    making some changes to the way I name things so I don't confusingly re-use variables that shouldn't be reused.  config files go in /etc not in the same place as scripts.

--- manifests/define_defaults.pp
+++ manifests/define_defaults.pp
@@ -6,14 +6,14 @@
 #
 define xtrabackup::define_defaults(
   $user,                     # The user to connect with
-  $pass,                     # The password to use
+  $password,                 # The password to use
   $port,                     # The port to use
   $socket,                   # The socket, they're not all the same
-  $scriptdir,                # The directory to stash the cnf
+  $confdir,                  # The directory to stash the cnf
   $datadir,                  # The main mysql directory
   $innodb_log_group_home_dir # The innodb log file dir
 ) {
-  file { "${scriptdir}/${title}":
+  file { "${confdir}/${title}":
     owner   => 'root',
     group   => 'root',
     mode    => 644,
