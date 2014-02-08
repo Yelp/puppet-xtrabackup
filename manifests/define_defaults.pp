4ff446ba6343a3d2e73d261428d799c42abacd33
Matt Ullmer <meu@yelp.com>
2014-02-07 16:07:42 -0800

    adding defaults to define_defaults.pp

--- manifests/define_defaults.pp
+++ manifests/define_defaults.pp
@@ -5,14 +5,14 @@
 # innodb_log_group_home_dir or other things explicitly
 #
 define xtrabackup::define_defaults(
-  $ensure = present,         # Ensure things present/absent
-  $user,                     # The user to connect with
-  $password,                 # The password to use
-  $port,                     # The port to use
-  $socket,                   # The socket, they're not all the same
-  $confdir,                  # The directory to stash the cnf
-  $datadir,                  # The main mysql directory
-  $innodb_log_group_home_dir # The innodb log file dir
+  $ensure                    = present, # Ensure things present/absent
+  $user                      = '',      # The user to connect with
+  $password                  = '',      # The password to use
+  $port                      = '',      # The port to use
+  $socket                    = '',      # The socket, they're not all the same
+  $confdir                   = '',      # The directory to stash the cnf
+  $datadir                   = '',      # The main mysql directory
+  $innodb_log_group_home_dir = ''       # The innodb log file dir
 ) {
   file { "${confdir}/${title}":
     ensure  => $ensure,
