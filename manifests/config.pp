441d27839f305f5a217bf637aa5d1f202efd8c5d
Matt Ullmer <meu@yelp.com>
2015-02-10 13:59:20 -0800

    add an asynchronous xtrabackup job to keep us at no more than 24 hours older than the oldest success file

--- manifests/config.pp
+++ manifests/config.pp
@@ -6,13 +6,19 @@ class xtrabackup::config {
   file { "$xtrabackup::scriptdir/mysql-xtrabackup":
     owner   => 'root',
     group   => 'root',
-    mode    => 744,
+    mode    => '744',
     content => template('xtrabackup/backupscript.sh.erb')
   }
   file { "$xtrabackup::scriptdir/mysql-xtrabackup-restore":
     owner   => 'root',
     group   => 'root',
-    mode    => 744,
+    mode    => '744',
     content => template('xtrabackup/restorescript.sh.erb')
+  }
+  file { "$xtrabackup::scriptdir/mysql-xtrabackup-async-prune":
+    owner   => 'root',
+    group   => 'root',
+    mode    => '744',
+    content => template('puppet:///modules/xtrabackup/asyncprune.sh')
   }
 }
