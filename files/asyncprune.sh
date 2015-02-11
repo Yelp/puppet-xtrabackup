29a608408bb7c365dae8f61cfb5440e8d4f88e11
Matt Ullmer <meu@yelp.com>
2015-02-10 17:44:15 -0800

    correct missed directory structure change

--- files/asyncprune.sh
+++ files/asyncprune.sh
@@ -17,10 +17,10 @@ prune_xtrabackups() {
   backup_root=$1
   cluster=$2
   current_timestamp="$(date +%s)"
-  backup_success_file="$(find ${backup_root}/${cluster} -maxdepth 1 -name "*xbstream.gz.success" | sort | tail -n 1 | head -n 1)"
+  backup_success_file="$(find ${backup_root}/${cluster}/xtrabackup -maxdepth 1 -name "*xbstream.gz.success" | sort | tail -n 1 | head -n 1)"
   successful_backup_ts="$(stat -c '%Y' "$backup_success_file")"
   tmpwatch_threshold=$(( ($current_timestamp - $successful_backup_ts) / 3600 + $wait_to_prune ))
-  tmpwatch -v -m "$tmpwatch_threshold" "${backup_root}/${cluster}"
+  tmpwatch -v -m "$tmpwatch_threshold" "${backup_root}/${cluster}/xtrabackup"
 }
 
 backup_root="$1"
