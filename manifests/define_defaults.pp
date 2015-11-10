9e3580d66fd5998fb3330c532d12eb878b12200f
Matt Ullmer <meu@yelp.com>
2015-11-09 16:31:19 -0800

    add open_file_limit configuration to the template so it can be set higher than 1024

--- manifests/define_defaults.pp
+++ manifests/define_defaults.pp
@@ -5,14 +5,15 @@
 # innodb_log_group_home_dir or other things explicitly
 #
 define xtrabackup::define_defaults(
-  $ensure                    = present, # Ensure things present/absent
-  $user                      = '',      # The user to connect with
-  $password                  = '',      # The password to use
-  $port                      = '',      # The port to use
-  $socket                    = '',      # The socket, they're not all the same
-  $confdir                   = '',      # The directory to stash the cnf
-  $datadir                   = '',      # The main mysql directory
-  $innodb_log_group_home_dir = ''       # The innodb log file dir
+  $ensure                      = present, # Ensure things present/absent
+  $user                        = '',      # The user to connect with
+  $password                    = '',      # The password to use
+  $port                        = '',      # The port to use
+  $socket                      = '',      # The socket, they're not all the same
+  $confdir                     = '',      # The directory to stash the cnf
+  $datadir                     = '',      # The main mysql directory
+  $innodb_log_group_home_dir   = '',      # The innodb log file dir
+  $xtrabackup_open_files_limit = ''       # The innobackupex hidden setting
 ) {
   file { "${confdir}/${title}":
     ensure  => $ensure,
