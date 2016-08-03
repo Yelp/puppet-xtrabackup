8cb59fa68a6024a702f3d2b9bad8d6675b54f5de
Matt Ullmer <meu@yelp.com>
2016-08-03 15:59:28 -0700

    update the percona-xtrabackup version

--- manifests/install.pp
+++ manifests/install.pp
@@ -3,7 +3,7 @@
 # Class to install xtrabackup from packages
 #
 class xtrabackup::install {
-  package { 'percona-xtrabackup':
+  package { 'percona-xtrabackup-24':
     ensure => $xtrabackup::version
   }
 }
