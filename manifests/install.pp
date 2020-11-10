5801ba42422e8c60bb9084759dd2c186917f7734
Alexander Martinez <alexanm@yelp.com>
2020-11-09 16:05:21 -0800

    updated install file to include mysql 8 support

--- manifests/install.pp
+++ manifests/install.pp
@@ -3,7 +3,11 @@
 # Class to install xtrabackup from packages
 #
 class xtrabackup::install {
-  if $::lsbdistcodename != 'lucid' {
+  if $xtrabackup::compatibility__version == 'mysql80' {
+    package {'percona-xtrabackup-80':
+      ensure => $xtrabackup::version
+    }
+  } elsif $::lsbdistcodename != 'lucid' {
     package { 'percona-xtrabackup-24':
       ensure => $xtrabackup::version
     }
